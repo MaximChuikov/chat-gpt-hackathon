23c1f568eb468b6bc96387bce074b6a63c590c8c
Slava <kruchkovenko@gmail.com>
2023-05-31 02:20:45 +0500

    add aboutMe

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import styles from './App.module.scss';
-import Backend from "./pages/back/Backend";
 import AboutMe from "@Pages/aboutMe/AboutMe";
 
 
@@ -9,7 +8,6 @@ const App = () => {
     return (
         <Routes>
             <Route path="*" element={<div className={styles.appBody}>error</div>}/>
-            <Route path="/back" element={<Backend/>}/>
             <Route path="/aboutMe" element={<AboutMe/>}/>
         </Routes>
     );
--- src/pages/aboutMe/AboutMe.tsx
+++ src/pages/aboutMe/AboutMe.tsx
@@ -1,30 +1,73 @@
 import React, {useCallback, useState} from "react";
 import styles from "./AboutMe.module.scss";
 import Header from "@Components/Header/Header";
+import UserController from "../../backend/controller/UserController";
 
 const AboutMe = () => {
     const states = [1, 2, 3];
-    const [curState, setCurState] = useState(1);
+    const [isError, setIsError] = useState(false)
+    const [curState, setCurState] = useState(3);
     const [name, setName] = useState("");
-    const [gender, setGender] = useState("");
+    const [gender, setGender] = useState<boolean>(true);
     const [interests, setInterests] = useState("");
-
-    const changeState = (state: number) => {
-        if(curState === 1){
-            if(name.length > 0){
-                setCurState(state);
-            }else{
-
-            }
+    const getState = () => {
+        switch (curState) {
+            case 1:
+                return getFirstCard();
+            case 2:
+                return getSecondCard();
+            case 3:
+                return getThirdCard();
+            default:
+                return getFirstCard();
         }
     }
+    const submit = useCallback(() => {
+        UserController.createUser({
+            name: name
+
+        })
+    }, [name, gender, interests]);
+    const changeState = useCallback((state: number) => {
+        if (curState === 1) {
+            if (name.length > 0) {
+                setCurState(state);
+            } else {
+                setIsError(true);
+            }
+        } else if (curState === 2) {
+            if (gender) {
+                setCurState(state);
+            } else {
+                setIsError(true);
+            }
+        } else if (curState === 3) {
+            if (interests.length > 0) {
+                setCurState(state);
+            } else {
+                setIsError(true);
+            }
+        }
+    }, [name, curState, gender, interests]);
     const getPoint = (state: number) => {
-        const onPointClick = useCallback(() => {
-            changeState(state)
-        }, [])
         return (
-            <div className={styles.point + " " + (state === curState && styles.selectPoint)} onClick={onPointClick}/>
+            <div className={styles.point + " " + (state === curState && styles.selectPoint)}
+                 onClick={() => changeState(state)}
+                 key={"state" + state.toString()}
+            />
         )
+    }
+    const changeName = (e: any) => {
+        setName(e.target.value);
+        setIsError(false);
+    }
+    const changeGender = (e: any) => {
+        setGender(e.target.value === "true");
+        setIsError(false);
+    }
+    const changeInterests = (e: any) => {
+        setInterests(e.target.value);
+        setIsError(false);
     }
     const getFirstCard = () => {
         return (<div className={styles.cardInfo}>
@@ -33,17 +76,83 @@ const AboutMe = () => {
             <input
                 type={"text"}
                 className={styles.nameFiled}
+                value={name}
+                onChange={changeName}
                 name="userName"
                 placeholder={"Имя Фамилия"}
             />
-            <button className={styles.nextBtn}>Далее →</button>
+            <button className={`${styles.nextBtn} ${styles.nextBtnEnd}`}
+                    onClick={() => changeState(2)}>
+                Далее →
+            </button>
+        </div>)
+    }
+    const getSecondCard = () => {
+        return (<div className={styles.cardInfo}>
+            <span className={styles.cardTitle}>Узнаем друг друга поближе? 👉👈</span>
+            <span className={styles.inputLabel}>Какой у тебя пол?</span>
+            <fieldset className={styles.genderFiled}>
+                <div className={styles.radioItem}>
+                    <input
+                        onChange={changeGender}
+                        className={styles.male}
+                        type={"radio"}
+                        value={"true"}
+                        name={"radio"}
+                        id={"male"}
+                        key={"male"}
+                        checked={gender}
+                    />
+                    <label>male</label>
+                </div>
+                <div className={styles.radioItem}>
+                    <input
+                        onChange={changeGender}
+                        className={styles.female}
+                        type={"radio"}
+                        value={"false"}
+                        name={"radio"}
+                        id={"female"}
+                        key={"female"}
+                        checked={!gender}
+                    />
+                    <label>female</label>
+                </div>
+            </fieldset>
+            <div className={styles.navigateBtns}>
+                <button className={styles.nextBtn} onClick={() => changeState(1)}>← Назад</button>
+                <button className={styles.nextBtn} onClick={() => changeState(3)}>Далее →</button>
+            </div>
+        </div>);
+    }
+    const getThirdCard = () => {
+        return (<div className={styles.cardInfo}>
+            <span className={styles.cardTitle}>Расскажешь о себе? 🥰</span>
+            <label htmlFor="userName" className={styles.inputLabel}>Напиши свои интересы</label>
+            <textarea
+                className={`${styles.interestFiled} ${styles.nameFiled}`}
+                value={interests}
+                onChange={changeInterests}
+                name="interests"
+                maxLength={140}
+                placeholder={"Я люблю кошечек 😺"}
+            />
+            <div className={styles.navigateBtns}>
+                <button className={styles.nextBtn} onClick={() => changeState(2)}>← Назад</button>
+                <button
+                    className={styles.nextBtn}
+                    onClick={() => {
+                    }}>
+                    Продолжить
+                </button>
+            </div>
         </div>)
     }
     return (
         <div className={styles.aboutMe}>
             <Header text={"Начальная анкета"}/>
             <div className={styles.card}>
-                {getFirstCard()}
+                {getState()}
             </div>
             <div className={styles.states}>
                 {
