3f3471c8dc80c694abc3d9574016ef8abeb0a3b8
Slava <kruchkovenko@gmail.com>
2023-05-30 14:21:16 +0500

    remove trash

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import styles from './App.module.scss';
-import Map from "./components/Map/Map";
-import 'leaflet/dist/leaflet.css'
 import Backend from "./pages/back/Backend";
 
 
-function App() {
+const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Map />}/>
             <Route path="*" element={<div className={styles.appBody}>error</div>}/>
             <Route path="/back" element={<Backend />}/>
         </Routes>
