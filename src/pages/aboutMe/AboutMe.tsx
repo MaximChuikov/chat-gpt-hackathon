1218dfbca3c456131e0895f5e1df195b53827f2d
Slava <kruchkovenko@gmail.com>
2023-05-30 21:13:56 +0500

    fix header

--- src/pages/aboutMe/AboutMe.tsx
+++ src/pages/aboutMe/AboutMe.tsx
@@ -4,7 +4,11 @@ import Header from "@Components/Header/Header";
 
 const AboutMe = () => {
     const states = [1, 2, 3];
-    const [curState, setCurState] = useState(1)
+    const [curState, setCurState] = useState(1);
+    const [name, setName] = useState("");
+    const [gender, setGender] = useState("");
+    const [interests, setInterests] = useState("");
+
     const getPoint = (state: number) => {
         const onPointClick = useCallback(() => {
             setCurState(state)
@@ -15,9 +19,13 @@ const AboutMe = () => {
     }
     return (
         <div className={styles.aboutMe}>
-            <Header text={"Форма"}/>
+            <Header text={"Начальная анкета"}/>
             <div className={styles.card}>
-
+                <div className={styles.cardInfo}>
+                    <span>Познакомимся?</span>
+                    <span>Введите ваше имя</span>
+                    <input type={"text"} value={name}/>
+                </div>
             </div>
             <div className={styles.states}>
                 {
