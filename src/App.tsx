aa22b23ab8d7ca6a421b23016b5cd7648daf31da
Seraphic <danilsocol-2002@mail.ru>
2023-05-31 02:31:43 +0500

    Кто сломает тому хурик в одно место

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NewEvent from "@Pages/NewEvent/NewEvent";
 import MatchPage from "@Pages/MatchPage/MatchPage";
 import Footer from "@Components/Footer/Footer";
 import AboutMe from "@Pages/aboutMe/AboutMe";
+import PersonalPage from "@Pages/personalPage/PersonalPage";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="/event" element={<NewEvent/>}/>
                 <Route path="/" element={<MatchPage/>}/>
                 <Route path="/aboutMe" element={<AboutMe/>}/>
+                <Route path="/personPage" element={<PersonalPage/>}/>
             </Route>
         </Routes>
     );
