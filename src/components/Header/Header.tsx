1594d28bdd50d367c9b6150170dd4ab101d44a52
Seraphic <danilsocol-2002@mail.ru>
2023-05-31 13:03:50 +0500

    styles update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import styles from "./Header.module.scss";
 const Header = ({text}:{text:string}) => {
     return(<div className={styles.header}>
         <span className={styles.headerText}>{text}</span>
+        <img src={"/img/icon_star.png"}/>
     </div>)
 }
 export default Header
