deeaa79d37ad452c3d5f473229b06de66658fd9f
Slava <kruchkovenko@gmail.com>
2023-05-31 12:44:47 +0500

    fix design

--- src/pages/MemePage/MemePage.tsx
+++ src/pages/MemePage/MemePage.tsx
@@ -20,7 +20,7 @@ const MemePage = () => {
                 <BarChart className={`${styles.icon} ${styles.top}`} onClick={openLikedPage}/>
             </div>
             <div className={styles.cardContainer}>
-                <Swiper swipeModels={users} onSwipe={MemeController.switchMeme} emptyText={"Мемов больше нет("}/>
+                <Swiper isVisible={false} swipeModels={users} onSwipe={MemeController.switchMeme} emptyText={"Мемов больше нет("}/>
             </div>
         </>
 
